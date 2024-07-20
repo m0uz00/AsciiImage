0a6e52fe03055315879c941b770bf9cc62bcf9c0
m0uz00 <mail@example.com>
2024-07-20 19:07:07 +0300

    setup

---
+++ processor.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import numpy as np
+import sys
+
+if len(sys.argv) < 2:
+    print("you need to provide the image path [python3 processor.py /image]")
+    sys.exit(1)
+else:
+    NotImplemented 
