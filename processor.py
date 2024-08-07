b99d85454bd35895561f421c309fbb2ae46a1e98
m0uz00 <mail@example.com>
2024-08-07 19:36:32 +0300

    Reapply "made it easier to call functions externally"
    This reverts commit daa4060c4de5c0aee94d28abddce01a05e470510.

--- processor.py
+++ processor.py
@@ -7,53 +7,58 @@ def normalise(min, max, value, char_len):
     return round(((value - min)/range)*char_len)
 
 
-if len(sys.argv) < 2:
-    print("you need to provide the image path [python3 processor.py /image]")
-    sys.exit(1)
-elif len(sys.argv) < 4:
-    x_scale = 8
-    y_scale = 12
-else:
-    x_scale = int(sys.argv[2])
-    y_scale = int(sys.argv[3])
+def convert(image_path, x_scale, y_scale):
+    try:
+        im = Image.open(image_path)
 
-image_path = sys.argv[1]
+    except FileNotFoundError:
+        print("This file doesn't exist")
+        sys.exit(1)
 
-try:
-    im = Image.open(image_path)
+    greyscale = im.convert("L")
 
-except FileNotFoundError:
-    print("This file doesn't exist")
-    sys.exit(1)
+    width, height = greyscale.size
+    height = round(height/y_scale)
+    width = round(width/x_scale)
+    greyscale = greyscale.resize((width, height))
 
-greyscale = im.convert("L")
+    greyscale.save("result.jpg")
 
-width, height = greyscale.size
-height = round(height/y_scale)
-width = round(width/x_scale)
-greyscale = greyscale.resize((width, height))
+    chars = [" ", ".", ":", "-", "c", "=", "+", "*", "o", "", "#", "%", "@"]
 
-greyscale.save("result.jpg")
+    comp = []
 
-chars = [" ", ".", ":", "-", "c", "=", "+", "*", "o", "P", "#", "%", "@"]
+    for line in range(height):
+        for column in range(width):
+            pixel = greyscale.getpixel((column, line))
+            comp.append(pixel)
 
-comp = []
+    minimum = min(comp)
+    maximum = max(comp)
 
-for line in range(height):
-    for column in range(width):
-        pixel = greyscale.getpixel((column, line))
-        comp.append(pixel)
+    output_file = open("result.txt", "w")
+    for line in range(height):
+        for column in range(width):
+            value = greyscale.load()[column, line]
+            pixel = chars[normalise(minimum, maximum, value, len(chars))-1]
+            print(pixel)
+            output_file.write(pixel)
+        output_file.write("\n")
 
-minimum = min(comp)
-maximum = max(comp)
+    output_file.close()
 
-output_file = open("result.txt", "w")
-for line in range(height):
-    for column in range(width):
-        value = greyscale.load()[column, line]
-        pixel = chars[normalise(minimum, maximum, value, len(chars))-1]
-        print(pixel)
-        output_file.write(pixel)
-    output_file.write("\n")
 
-output_file.close()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("you need to provide the image path [python3 processor.py /]")
+        sys.exit(1)
+    elif len(sys.argv) < 4:
+        x_scale = 8
+        y_scale = 12
+    else:
+        x_scale = int(sys.argv[2])
+        y_scale = int(sys.argv[3])
+
+    image_path = sys.argv[1]
+
+    convert(image_path, x_scale, y_scale)
